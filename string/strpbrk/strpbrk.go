7cf54fc2851aa9a737f06d33b7935ba0ee623e3a
qjq <1376557125@qq.com>
2021-09-14 20:06:32 +0800

    Modify conditions and judgments

--- string/strpbrk/strpbrk.go
+++ string/strpbrk/strpbrk.go
@@ -2,11 +2,11 @@ package strpbrk
 
 import "strings"
 
-func Do(haystack string, charList string) interface{} {
-	if len(charList) == 0 {
+func Do(haystack ...string) interface{} {
+	if len(haystack) < 2 {
 		return false
 	}
-	r := strings.Index(haystack, charList)
+	r := strings.Index(haystack[0], haystack[1])
 	if r == -1 {
 		return false
 	}
