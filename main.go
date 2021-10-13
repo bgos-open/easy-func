065d20d701e47a301c9a1f36fc9230ae7a0b059e
刘景城 <liujingcheng@banggood.com>
2021-10-13 18:11:32 +0800

    reset main.go

--- main.go
+++ main.go
@@ -1,14 +1,1 @@
-package main
-
-import (
-	"easy-func/string/stristr"
-	"fmt"
-)
-
-func main(){
-	s, err := stristr.Do("fasfasdf 333", "33" , true)
-	if err != nil {
-		println(err.Error())
-	}
-	fmt.Println(s)
-}
+package main 
