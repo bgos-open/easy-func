0e43d9e32960395ed78da34a16dd6266eddc86a1
vincent <1193569812@qq.com>
2021-10-16 15:15:47 +0800

    add func strcasecmp

---
+++ string/strcasecmp/strcasecmp.go
@@ -0,0 +1,13 @@
+package strcasecmp
+
+import (
+	"strings"
+)
+
+// https://www.php.net/manual/en/function.strcasecmp
+func Do(string1 string, string2 string) int {
+	if strings.EqualFold(string1, string2){
+		return 0
+	}
+	return 1
+}
