083551baa098bb6ae39e46eb0d293f7b1f92f8d2
alphanoah <noahbin0829@gmail.com>
2022-03-25 00:40:30 +0800

    Add  test case of function chr and add benchmark of md5

--- string/md5/md5.go
+++ string/md5/md5.go
@@ -3,11 +3,42 @@ package md5
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
+	"io"
 )
 
-// https://www.php.net/manual/en/function.md5
-func Do(s string) string {
-	hasher := md5.New()
-	hasher.Write([]byte(s))
-	return hex.EncodeToString(hasher.Sum(nil))
+// Do https://www.php.net/manual/en/function.md5
+func Do(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+/*
+Comparison of benchmark test results of three type methods
+goos: windows
+goarch: amd64
+pkg: easy-func/string/md5
+cpu: Intel(R) Core(TM) i5-6500 CPU @ 3.20GHz
+BenchmarkDo
+BenchmarkDo-4    	 2105058	       567.5 ns/op
+BenchmarkDo2
+BenchmarkDo2-4   	  876865	      1391 ns/op
+BenchmarkDo3
+BenchmarkDo3-4   	 1328520	       888.6 ns/op
+PASS
+*/
+
+func Do2(str string) string {
+	data := []byte(str)
+	has := md5.Sum(data)
+	md5str := fmt.Sprintf("%x", has)
+	return md5str
+}
+
+func Do3(str string) string {
+	w := md5.New()
+	io.WriteString(w, str)
+	md5str := fmt.Sprintf("%x", w.Sum(nil))
+	return md5str
 }
